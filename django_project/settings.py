8bf13d2db624071a954ff61e08515f55acd92dc6
Fatlum Llumnica <108372677+fatlumllumnica@users.noreply.github.com>
2022-10-27 16:54:18 +0200

    APIs created

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,9 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',\
-    'books', #local apps
-    'rest_framework', #3d party apps
+    'django.contrib.staticfiles',
+    #3d party apps
+    'rest_framework', 
+    #local apps
+    'books', 
+    'apis',
 ]
 
 MIDDLEWARE = [
